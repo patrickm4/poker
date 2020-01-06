fbb4368229b9c4f91ec0e33c4d43c24d1965bfab
karizmo <32525686+karizmo@users.noreply.github.com>
2020-01-06 15:21:44 -0800

    WIP suits

--- src/components/hand.js
+++ src/components/hand.js
@@ -12,11 +12,39 @@ const Hand = forwardRef((props, ref) => {
     doDraw() {
       const diffHand = hand.map(e=>{
         //TODO need to add suits to the numbers
-        let card = Math.round(Math.random() * 13 + 1);
+        // let card = Math.round(Math.random() * 12 + 1);
+        let card = {
+          value: Math.round(Math.random() * 12 + 1),
+          suit: Math.round(Math.random() * 3 + 1)
+        }
         return e = card
       })
-      console.log("yaaw", diffHand)
       setHand(diffHand)
+      const newHand = diffHand.map(f=>{
+        console.log("yeet", f, typeof f, f.value, f.suit)
+        //face cards and ace
+        if(f.value === 11){
+          // return f.value = 'J'
+          let oneCard = {value:'J',suit:f.suit}
+          return oneCard
+        } else if(f.value === 12){
+          // return f.value = 'Q'
+          let oneCard = {value:'Q',suit:f.suit}
+          return oneCard
+        } else if(f.value === 13){
+          // return f.value = 'K'
+          let oneCard = {value:'K',suit:f.suit}
+          return oneCard
+        } else if(f.value === 1){
+          // return f.value = 'A'
+          let oneCard = {value:'A',suit:f.suit}
+          return oneCard
+        } else {
+          return {value: f.value, suit:f.suit}
+        }
+      })
+      console.log("yaaw", newHand)
+      setDisplayHand(newHand)
       //TODO update displayHand UI with new hand
     }
     // TODO add holding cards
@@ -24,11 +52,11 @@ const Hand = forwardRef((props, ref) => {
 
   return (
     <div>
-      <span className="card">{hand[0] === 11 ? 'J' : hand[0] }</span>
-      <span className="card">{hand[1]}</span>
-      <span className="card">{hand[2]}</span>
-      <span className="card">{hand[3]}</span>
-      <span className="card">{hand[4]}</span>
+      <span className="card">{!displayHand[0].value ? '0' : displayHand[0].value}</span>
+      <span className="card">{!displayHand[1].value ? '0' : displayHand[1].value}</span>
+      <span className="card">{!displayHand[2].value ? '0' : displayHand[2].value}</span>
+      <span className="card">{!displayHand[3].value ? '0' : displayHand[3].value}</span>
+      <span className="card">{!displayHand[4].value ? '0' : displayHand[4].value}</span>
     </div>
   );
 });
