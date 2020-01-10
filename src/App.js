b849691d8612e2cac7c6d27601b25125a9045864
karizmo <32525686+karizmo@users.noreply.github.com>
2020-01-09 20:52:55 -0800

    add non clickable cards after second draw

--- src/App.js
+++ src/App.js
@@ -6,26 +6,26 @@ function App() {
   // build cards (number and suit)
   // build hands 5 or 3
   // build winning
-  // let dNum = 0
-  const [dNum, setDNum] = useState(0)
+  // let drawNumber = 0
+  const [drawNumber, setdrawNumber] = useState(0)
   const mainHand = useRef();
   const [mainBtn, setMainBtn] = useState('Draw')
 
   // TODO add function so user can only draw twice before needed to reset
 
   const handleDraw = () =>{
-    if(dNum === 1){
+    if(drawNumber === 1){
       console.log("asd")
       setMainBtn('Reset')
     }
-    if(dNum < 2){
+    if(drawNumber < 2){
       mainHand.current.doDraw()
-      // dNum += 1
-      setDNum(dNum + 1)
+      // drawNumber += 1
+      setdrawNumber(drawNumber + 1)
     } else {
       setMainBtn('Draw')
       mainHand.current.reDraw()
-      setDNum(1)
+      setdrawNumber(1)
     }
   }
 
@@ -38,8 +38,8 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Poker</h1>
-      <div className="hand">
+      <h1>Poker Deuces Wild</h1>
+      <div className={ drawNumber === 2 ? "hand doneDraw" : "hand" }>
         <Hand ref={mainHand} />
       </div>
       <DrawButton />
